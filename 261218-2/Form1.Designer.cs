9414a080ecd041b9cd497560646fe56081c85320
sagitush <44786027+sagitush@users.noreply.github.com>
2019-01-01 16:21:03 +0200

    Add files via upload

---
+++ 261218-2/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace _261218_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumberLbl = new System.Windows.Forms.Label();
+            this.NextBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NumberLbl
+            // 
+            this.NumberLbl.AutoSize = true;
+            this.NumberLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.NumberLbl.Location = new System.Drawing.Point(352, 185);
+            this.NumberLbl.Name = "NumberLbl";
+            this.NumberLbl.Size = new System.Drawing.Size(25, 25);
+            this.NumberLbl.TabIndex = 0;
+            this.NumberLbl.Text = "?";
+            // 
+            // NextBtn
+            // 
+            this.NextBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.NextBtn.Location = new System.Drawing.Point(339, 70);
+            this.NextBtn.Name = "NextBtn";
+            this.NextBtn.Size = new System.Drawing.Size(233, 46);
+            this.NextBtn.TabIndex = 1;
+            this.NextBtn.Text = "The next number is:";
+            this.NextBtn.UseVisualStyleBackColor = true;
+            this.NextBtn.Click += new System.EventHandler(this.NextBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NextBtn);
+            this.Controls.Add(this.NumberLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NumberLbl;
+        private System.Windows.Forms.Button NextBtn;
+    }
+}
+
---
+++ 261218-2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _261218_2
+{
+    public partial class Form1 : Form
+    {
+        int[] Randomarray = new int[10];
+        int sum=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            Random r = new Random();
+            for (int i = 0; i < Randomarray.Length; i++)
+            {
+                Randomarray[i] = r.Next(101);
+
+            }
+            
+
+        }
+
+        private void NextBtn_Click(object sender, EventArgs e)
+        {
+            if (sum < Randomarray.Length)
+            {
+                if (sum != Randomarray.Length - 1)
+                {
+                    NumberLbl.Text = $"The number is:{Randomarray[sum]}";
+                    sum = sum + 1;
+                }
+                else
+                {
+                    NumberLbl.Text = $"The number is:{Randomarray[sum]}";
+                    sum = sum + 1;
+                    NumberLbl.BackColor = Color.Red;
+                }
+            }
+            
+        }
+    }
+}
